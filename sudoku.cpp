11e45ea3f00e08a2a0b21c60f85ef3f1add88b27
Charlie Medina <charliegmv@hotmail.com>
2013-06-25 21:43:34 -0500

    Cuarto avance
    1era implementación de algoritmo generador de tablero

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,6 +1,10 @@
 #include "sudoku.h"
 #include "ui_sudoku.h"
+#include <stdlib.h>
+#include <time.h>
 #include <iostream>
+#include <QGlobal.h>
+#include <QTime>
 
 
 Sudoku::Sudoku(QWidget *parent) :
@@ -8,7 +12,9 @@ Sudoku::Sudoku(QWidget *parent) :
     ui(new Ui::Sudoku)
 {
     ui->setupUi(this);
+    srand((unsigned)time(NULL));
     llenarTabla();
+    rellenarWidget();
 
 }
 
@@ -29,9 +35,90 @@ void Sudoku::llenarTabla()
             z++;
         }
     }
+
+    for(int k=0; k<9; k++){
+        for(int l=0; l<9; l++){
+            matriz[k][l]=0;
+        }
+    }
+    asignarFila(0);
+}
+
+void Sudoku::asignarFila(int fila){
+    if (fila>8){
+        return;
+    }
+    else{
+        //Crear un arreglo de 9 ints, y le asigna los valores 1,2,...9
+        int arreglo[9];
+        for(int i=1;i<=9;i++){
+            arreglo[i-1]=i;
+        }
+
+        //Una vez generada la fila, los intercambia de posiciones 9 veces, para que se desordene
+        int rango=9;
+        while (rango > 0){
+            int random = rand()%rango;
+            rango--;
+            int temp = arreglo[rango];
+            arreglo[rango]=arreglo[random];
+            arreglo[random] = temp;
+        }
+
+        //Llena la fila de la matriz con los valores del arreglo
+        bool flag = false;
+        for(int j=0; j<9; j++){
+            for(int k=0; k<9; k++){
+                flag=false;
+                if(arreglo[k]>=0){
+                    flag = asignarNumero(fila, j, arreglo[k], 0);
+                }
+
+                if(flag==true){
+                    arreglo[k]=-1;
+                    break;
+                }
+            }
+        }
+        asignarFila(fila + 1);
+    }
+
+}
+
+bool Sudoku::asignarNumero(int fila, int columna, int valor, int counter){
+
+    if(counter>columna){
+        return false;
+    }
+
+    if(validarValor(fila,columna,valor) && !counter) {
+        matriz[fila][columna] = valor;
+        return true;
+    }
+    else if(counter && validarValor(fila,columna - counter,valor) && validarValor(fila, columna, matriz[fila][columna - counter])){
+        matriz[fila][columna] = matriz[fila][columna - counter];
+        matriz[fila][columna - counter] = valor;
+        return true;
+    }else {
+        counter++;
+        return asignarNumero(fila, columna, valor, counter);
+    }
 }
 
 
+void Sudoku::rellenarWidget(){
+    QLineEdit *ledit;
+
+    //rellenarMatriz(0,0);
+    for(int i=0; i<9; i++)
+    {
+        for(int j=0; j<9; j++)
+        {
+            ledit = (QLineEdit*)(ui->gridTabla->itemAtPosition(i,j)->widget());
+            ledit->setText(QString::number(matriz[i][j]));
+        }
+    }
+}
 
 void Sudoku::on_Btn_validar_clicked()
 {
@@ -61,6 +148,27 @@ void Sudoku::on_Btn_validar_clicked()
 
     if(!valido) std::cout<<"El tablero esta mal llenado"<<std::endl;
     else std::cout<<"El tablero esta bien llenado"<<std::endl;
+}
+
+bool Sudoku::validacion(int fila, int columna){
+    if(validarFila(fila,columna) && validarColumna(fila,columna) && validarBloque(fila,columna)) return true;
+    return false;
+}
+
+bool Sudoku::validarValor(int fila, int columna, int valor){
+    int centrox = fila/3;
+    int centroy = columna/3;
+    for(int i=fila; i<=9; i++){
+        for(int j=0; j<=9; j++){
+            if(matriz[i][j]!=0){
+                if(fila==i || columna==j || centrox==i/3 || centroy==j/3){
+                    if(matriz[i][j]==valor)
+                        return false;
+                }
+            }
+        }
+    }
+    return true;
 }
 
 bool Sudoku::validarFila(int fila, int columna){
--- sudoku.h
+++ sudoku.h
@@ -15,15 +15,20 @@ class Sudoku;
 class Sudoku : public QMainWindow
 {
     Q_OBJECT
-    
+
 public:
     explicit Sudoku(QWidget *parent = 0);
     void llenarTabla();
+    bool validacion(int fila, int columna);
     bool validarFila(int fila, int columna);
     bool validarColumna(int fila, int columna);
     bool validarBloque(int fila, int columna);
+    bool validarValor(int fila, int columna, int valor);
+    void rellenarWidget();
+    void asignarFila(int fila);
+    bool asignarNumero(int fila, int columna, int valor, int counter);
     ~Sudoku();
-    
+
 private slots:
     void on_Btn_validar_clicked();
 
